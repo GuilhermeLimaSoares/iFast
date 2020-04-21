2f26354004c7b31f686bbd930c453031c2908ac1
GuilhermeLimaSoares <guilhermelimasoares9@gmail.com>
2020-04-21 01:05:34 -0300

    CAROUSSEL: Criando componente de carossuel

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,26 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { HomeComponent } from './pages/home/home.component';
+import { AboutComponent } from './pages/about/about.component';
+import { ChampionshipComponent } from './pages/championship/championship.component';
+import { GalleryComponent } from './pages/gallery/gallery.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { SectionComponent } from './components/section/section.component';
+import { CarousselComponent } from './components/caroussel/caroussel.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    HomeComponent,
+    AboutComponent,
+    ChampionshipComponent,
+    GalleryComponent,
+    FooterComponent,
+    SectionComponent,
+    CarousselComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/components/caroussel/caroussel.component.ts
@@ -0,0 +1,31 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-caroussel',
+  templateUrl: './caroussel.component.html',
+  styleUrls: ['./caroussel.component.scss']
+})
+export class CarousselComponent implements OnInit {
+  currentIndex = 0;
+  currentImage: string;
+  pathImage = "src/assets/";
+  imageArray = [`${this.pathImage}gamer.jpg`, `${this.pathImage}sol.jpg`, `${this.pathImage}wolf.jpg`];
+  constructor() {
+
+  }
+
+  ngOnInit(): void {
+    this.currentImage = this.imageArray[0];
+    this.changeCarousselImage();
+  }
+
+  changeCarousselImage() {
+    let indexImage = 0;
+    setInterval(() => {
+      this.currentIndex = this.currentIndex < this.imageArray.length - 1 ? this.currentIndex + 1 : this.currentIndex = 0;
+      this.currentImage = this.imageArray[this.currentIndex];
+      console.log(this.currentImage);
+    }, 5000);
+  }
+
+}
